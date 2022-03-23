bfd12a18fbd95c9644147e1ed8b9ae7858942621
SirGibihm <D.Holin94@gmail.com>
2022-03-23 17:53:03 +0100

    syntax of setup.py was faulty

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='sftp_pypeline',
     version='2.0.0',
-    description="Quickly set up a robust sftp console based SFTP Downloaded/Uploader"
-    author="David Holin"
-    url="https://github.com/SirGibihm"
+    description="Quickly set up a robust sftp console based SFTP Downloaded/Uploader",
+    author="David Holin",
+    url="https://github.com/SirGibihm",
     packages=find_packages(include=['sftp_pypeline']),
     install_requires=[
         "paramiko>=2.10.2",
         "psutil>=5.9.0"
         ],
     entry_points={
-        'console_scripts': ['sftp_pypeline=sftp_pypeline.sftp_pypeline:main']
+        'console_scripts': ['sftp_pypeline=sftp_pypeline.sftp_pypeline:main'],
     },
     setup_requires=['flake8']
 )
